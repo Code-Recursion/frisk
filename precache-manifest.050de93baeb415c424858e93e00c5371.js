1dba8cfa94e2bbfcae5c1a669683da5a2623762a
Ajay Singh <helloajaysingh1@gmail.com>
2020-11-22 21:24:57 +0530

    Updates

---
+++ precache-manifest.050de93baeb415c424858e93e00c5371.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ae8e42297b6a454ccdcc2edf2c6ee6",
+    "url": "/frisk/index.html"
+  },
+  {
+    "revision": "bc5f92689a45aab2aa5c",
+    "url": "/frisk/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8eb0916109de2af739d3",
+    "url": "/frisk/static/css/main.1127ccc9.chunk.css"
+  },
+  {
+    "revision": "bc5f92689a45aab2aa5c",
+    "url": "/frisk/static/js/2.32606063.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/frisk/static/js/2.32606063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb0916109de2af739d3",
+    "url": "/frisk/static/js/main.81471aa2.chunk.js"
+  },
+  {
+    "revision": "7b3d55aedefe1fb00b7c",
+    "url": "/frisk/static/js/runtime-main.82a67fe4.js"
+  },
+  {
+    "revision": "3cfa25ed1b379620497d55973ccc65a6",
+    "url": "/frisk/static/media/covid-logo.3cfa25ed.png"
+  },
+  {
+    "revision": "4f8369a7e073b9bdbab92c463fd47437",
+    "url": "/frisk/static/media/safety.4f8369a7.jpg"
+  }
+]);
